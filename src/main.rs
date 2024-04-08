c282297049ec9354f7314797e5d5a85c8903d31c
gurupatoh <patrickn0023@gmail.com>
2024-04-08 22:58:24 +0300

    summation commit

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
 fn main() {
-    println!("Hello, world!");
+let results=summation(7,9);
+    display_results(results);
+
+}
+fn summation(a:i32,b:i32)->i32{
+    a+b
+}
+fn display_results(results:i32){
+    println!("The results are:{:?}",results)
 }
